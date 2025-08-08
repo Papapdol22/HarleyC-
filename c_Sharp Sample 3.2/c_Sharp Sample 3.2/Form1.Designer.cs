4c7acaedb91a89c79cccda28e946e370685e145f
Papapdol22 <gilbertbaylosis8@gmail.com>
2025-08-08 14:17:24 +0800

    Add files via upload

---
+++ c_Sharp Sample 3.2/c_Sharp Sample 3.2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace c_Sharp_Sample_3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdLanguage = new System.Windows.Forms.ComboBox();
+            this.btnGreet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "C_Sharp Sample 3.2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(149, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select a Language";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cmdLanguage
+            // 
+            this.cmdLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdLanguage.FormattingEnabled = true;
+            this.cmdLanguage.Items.AddRange(new object[] {
+            "English ",
+            "Tagalog",
+            "Hiligaynon",
+            "Bisaya"});
+            this.cmdLanguage.Location = new System.Drawing.Point(293, 84);
+            this.cmdLanguage.Name = "cmdLanguage";
+            this.cmdLanguage.Size = new System.Drawing.Size(121, 24);
+            this.cmdLanguage.TabIndex = 2;
+            this.cmdLanguage.Text = "Language";
+            this.cmdLanguage.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnGreet
+            // 
+            this.btnGreet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGreet.Location = new System.Drawing.Point(420, 84);
+            this.btnGreet.Name = "btnGreet";
+            this.btnGreet.Size = new System.Drawing.Size(75, 23);
+            this.btnGreet.TabIndex = 3;
+            this.btnGreet.Text = "Greet";
+            this.btnGreet.UseVisualStyleBackColor = true;
+            this.btnGreet.Click += new System.EventHandler(this.btnGreet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGreet);
+            this.Controls.Add(this.cmdLanguage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmdLanguage;
+        private System.Windows.Forms.Button btnGreet;
+    }
+}
+
---
+++ c_Sharp Sample 3.2/c_Sharp Sample 3.2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_Sharp_Sample_3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGreet_Click(object sender, EventArgs e)
+        {
+            string language = cmdLanguage.Text.Trim().ToLower();
+            string greet = "";
+
+            
+            if (language == "english")
+            {
+                greet = "Good Morning.";
+            }
+            else if (language == "tagalog")
+            {
+                greet = "Magandang Umaga.";
+            }
+            else if (language == "hiligaynon")
+            {
+                greet = "Maayong Aga.";
+            }
+            else if (language == "bisaya")
+            {
+                greet = "Maayong Buntag.";
+            }
+
+          
+            greet += " Gilbert";
+
+            
+            MessageBox.Show(greet);
+
+
+        }
+    }
+}
